d1e67c668e3d040d73652861a2bc00f52af9ec86
jashonmichael <jashon.michael@student.moringaschool.com>
2024-03-21 08:18:32 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+
+    bestCustomer = 'not bob';
+}
+
+function ovewriteBestCustomer(){
+
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavouriteCustomer(){
+
+const leastFavouriteCustomer = 'not bob';
+leastFavouriteCustomer = 'bob';
+}
